8c3419568efdd584759e700e668343ef90f27ef2
Brock Berrett <broxhouse@gmail.com>
2016-07-21 08:44:39 -0600

    ehhe

--- src/main/java/com/broxhouse/ApiFactory.java
+++ src/main/java/com/broxhouse/ApiFactory.java
@@ -1,20 +1,40 @@
 package com.broxhouse;
 
 import retrofit2.Retrofit;
+import javax.ws.rs.core.Response;
+
 
 
 /**
  * Created by Brock Berrett on 7/20/2016.
  */
 public class ApiFactory
+
+private static class ApiKeyInterceptor implements Interceptor {
+
+    private final String apiKey;
+
+    private ApiKeyInterceptor(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Request newRequest = request.newBuilder()
+                .addHeader("Ocp-Apim-Subscription-Key", apiKey)
+                .build();
+        System.out.println(newRequest.httpUrl());
+        return chain.proceed(newRequest);
+    }
 {
-//    public ApiFactory(String apiKey) {
-//        retrofit = new Retrofit.Builder()
-//                .baseUrl("https://www.haloapi.com")
-//                .addConverterFactory(JacksonConverterFactory.create())
-//                .build();
-//        interceptor = new ApiKeyInterceptor(apiKey);
-//        retrofit.client().setFollowRedirects(false);
-//        retrofit.client().interceptors().add(interceptor);
-//    }
+    public ApiFactory(String apiKey) {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.haloapi.com")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+        interceptor = new ApiKeyInterceptor(apiKey);
+        retrofit.client().setFollowRedirects(false);
+        retrofit.client().interceptors().add(interceptor);
+    }
 }
